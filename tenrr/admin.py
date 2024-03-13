495cfd25326887a974927d8d0293ec9013db8dc3
Colin Nardo <2795201n@student.gla.ac.uk>
2024-03-13 19:52:41 +0000

    Re formatted htmls by adding a base.html. Fully implemented login and singup account creation, adding logout

--- tenrr/admin.py
+++ tenrr/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from tenrr.models import UserProfile
+# # Register your models here.
 
-# Register your models here.
+# admin.site.register(UserProfile)
+
